6a5d551296f424b8e6004c88a3dd78adc98c87e2
prafullprince <prafullprince12@gmail.com>
2024-03-01 14:17:33 +0530

    day 1

---
+++ controllers/Auth.js
@@ -0,0 +1,7 @@
+// sendOtp
+
+// signup
+
+// login
+
+// changePassword
--- models/Otp.js
+++ models/Otp.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
 
 const otpSchema = new mongoose.Schema({
     email:{
@@ -16,4 +17,22 @@ const otpSchema = new mongoose.Schema({
         expires:5*60,
     },
 });
+
+//  a function to send email
+async function sendVerificationMail(email,otp){
+    try{
+        const mailResponse = await mailSender(email,"verification email from studyNotion",otp);
+        console.log("email sent successfully",mailResponse);
+
+    }catch(error){
+        console.log("error while sending mail" , error);
+        throw error;
+    }
+}
+
+otpSchema.pre("save",async function(next){
+    await sendVerificationMail(this.email,this.otp);
+    next();
+})
+
 module.exports = mongoose.model("OTP",otpSchema);
